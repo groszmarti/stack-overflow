521ef9d6b25b55e190c41043bcd4f796de86ffb0
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-05-23 10:31:43 +0200

    list all questions on main page

--- client/stack-over-flow-number-2/src/Pages/MainPage.jsx
+++ client/stack-over-flow-number-2/src/Pages/MainPage.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react'
 
 const MainPage = () => {
 
+  const [allQuestion, setAllQuestion] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/questions/all')
+    .then(res => res.json())
+    .then(data => setAllQuestion(data));
+
+  }, []);
+
+  if(allQuestion.length === 0){
+    return <div>Loading...</div>
+  }
+
   return <>
-  <div>Hello</div>
+  {allQuestion.map(question => {console.log(question)
+    return <div>{`${question.title} - ${question.created}`}</div>
+  } )}
   </>
 };
 
